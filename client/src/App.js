b13636d68f11d13304938ce0aaf5ca06649d95d1
Vaibhav <vaibhavverma21122001@gmai.com>
2022-11-11 15:29:40 +0530

    removed loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-// import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
